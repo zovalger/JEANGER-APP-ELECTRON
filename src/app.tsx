92a5a1dfe6767711b05fd4e4843c1d228ac15ca3
zovalger <zovalger@gmail.com>
2025-05-27 12:43:09 -0400

    setup

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const root = createRoot(document.body);
+
+root.render(
+	<>
+		<h2 className="px-2">hola</h2>
+		
+		<h1 className="text-gray-700 font-bold text-3xl">gola</h1>
+	</>
+);
