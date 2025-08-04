f2efa96af28f969e7edf7e9f72fa06cb340f00d3
zovalger <zovalger@gmail.com>
2025-08-03 23:40:20 -0400

    Adds  `patch` method to HTTP

--- src/common/config/HttpClient.ts
+++ src/common/config/HttpClient.ts
@@ -1,103 +1,114 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 interface Options {
-    token?: string;
+	token?: string;
 }
 
 interface ServerResponse<D> {
-    // todo: colocar los demas atributos de la respuesta del server
-    data: D;
+	// todo: colocar los demas atributos de la respuesta del server
+	data: D;
 }
 
 interface ServerError {
-    message: string[];
-    error: string;
-    statusCode: number;
+	message: string[];
+	error: string;
+	statusCode: number;
 }
 
 class HttpClient {
-    private instance;
+	private instance;
 
-    constructor(baseURL: string, options?: Options) {
-        this.instance = axios.create({
-            baseURL,
-            // withCredentials: true,
-        });
+	constructor(baseURL: string, options?: Options) {
+		this.instance = axios.create({
+			baseURL,
+			// withCredentials: true,
+		});
 
-        this.instance.interceptors.request.use((config) => {
-            if (options?.token) {
-                config.headers["Authorization"] = `Bearer ${options.token}`;
-            }
-            return config;
-        });
+		this.instance.interceptors.request.use((config) => {
+			if (options?.token) {
+				config.headers["Authorization"] = `Bearer ${options.token}`;
+			}
+			return config;
+		});
 
-        this.instance.interceptors.response.use(
-            (response) => response,
-            (error) => {
-                if (error.response) {
-                    const serverError: ServerError = {
-                        message: error.response.data.message || ["An unknown error occurred"],
-                        error: error.response.data.error || "Unknown Error",
-                        statusCode: error.response.status || 500,
-                    };
-                    return Promise.reject(serverError);
-                }
-                return Promise.reject({
-                    message: ["Network error or no response from server"],
-                    error: "Network Error",
-                    statusCode: 0,
-                });
-            }
-        );
-    }
+		this.instance.interceptors.response.use(
+			(response) => response,
+			(error) => {
+				if (error.response) {
+					const serverError: ServerError = {
+						message: error.response.data.message || [
+							"An unknown error occurred",
+						],
+						error: error.response.data.error || "Unknown Error",
+						statusCode: error.response.status || 500,
+					};
+					return Promise.reject(serverError);
+				}
+				return Promise.reject({
+					message: ["Network error or no response from server"],
+					error: "Network Error",
+					statusCode: 0,
+				});
+			}
+		);
+	}
 
-    async get<T>(
-        url: string,
-        config?: AxiosRequestConfig
-    ): Promise<ServerResponse<T>> {
-        const response: AxiosResponse<ServerResponse<T>> = await this.instance.get(
-            url,
-            config
-        );
+	async get<T>(
+		url: string,
+		config?: AxiosRequestConfig
+	): Promise<ServerResponse<T>> {
+		const response: AxiosResponse<ServerResponse<T>> = await this.instance.get(
+			url,
+			config
+		);
 
-        return response.data;
-    }
+		return response.data;
+	}
 
-    async post<T>(
-        url: string,
-        data?: unknown,
-        config?: AxiosRequestConfig
-    ): Promise<ServerResponse<T>> {
-        const response: AxiosResponse<ServerResponse<T>> = await this.instance.post(
-            url,
-            data,
-            config
-        );
-        return response.data;
-    }
+	async post<T>(
+		url: string,
+		data?: unknown,
+		config?: AxiosRequestConfig
+	): Promise<ServerResponse<T>> {
+		const response: AxiosResponse<ServerResponse<T>> = await this.instance.post(
+			url,
+			data,
+			config
+		);
+		return response.data;
+	}
 
-    async put<T>(
-        url: string,
-        data?: unknown,
-        config?: AxiosRequestConfig
-    ): Promise<ServerResponse<T>> {
-        const response: AxiosResponse<ServerResponse<T>> = await this.instance.put(
-            url,
-            data,
-            config
-        );
-        return response.data;
-    }
+	async put<T>(
+		url: string,
+		data?: unknown,
+		config?: AxiosRequestConfig
+	): Promise<ServerResponse<T>> {
+		const response: AxiosResponse<ServerResponse<T>> = await this.instance.put(
+			url,
+			data,
+			config
+		);
+		return response.data;
+	}
 
-    async delete<T>(
-        url: string,
-        config?: AxiosRequestConfig
-    ): Promise<ServerResponse<T>> {
-        const response: AxiosResponse<ServerResponse<T>> =
-            await this.instance.delete(url, config);
-        return response.data;
-    }
-    
+	async patch<T>(
+		url: string,
+		data?: unknown,
+		config?: AxiosRequestConfig
+	): Promise<ServerResponse<T>> {
+		const response: AxiosResponse<ServerResponse<T>> =
+			await this.instance.patch(url, data, config);
+		return response.data;
+	}
+
+	async delete<T>(
+		url: string,
+		config?: AxiosRequestConfig
+	): Promise<ServerResponse<T>> {
+		const response: AxiosResponse<ServerResponse<T>> =
+			await this.instance.delete(url, config);
+		return response.data;
+	}
 }
 
 export default HttpClient;
