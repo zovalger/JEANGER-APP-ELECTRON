5729cb615dbd5522cd3cd4127fa757848ddfe32e
zovalger <zovalger@gmail.com>
2025-09-01 07:26:31 -0400

    Adds sheet size definitions for PDF generation

--- src/pdf_tools/hooks/usePdfWeb.ts
+++ src/pdf_tools/hooks/usePdfWeb.ts
@@ -9,6 +9,44 @@ export interface ImgToPdf {
 	rotation: number;
 }
 
+export enum SheetType {
+	Letter,
+	office,
+	extreoffice,
+	A0,
+	A1,
+	A2,
+	A3,
+	A4,
+	A5,
+	A6,
+	A7,
+	A8,
+	A9,
+	A10,
+}
+
+interface SheetSizeObject {
+	[key: string]: { width: number; height: number };
+}
+
+export const SheetSize: SheetSizeObject = {
+	Letter: { width: 2480, height: 3508 },
+	office: { width: 2480, height: 3508 },
+	extreoffice: { width: 2480, height: 3508 },
+	A0: { width: 9933, height: 14043 },
+	A1: { width: 7016, height: 9933 },
+	A2: { width: 4961, height: 7016 },
+	A3: { width: 3508, height: 4961 },
+	A4: { width: 2480, height: 3508 },
+	A5: { width: 1748, height: 2480 },
+	A6: { width: 1240, height: 1748 },
+	A7: { width: 874, height: 1240 },
+	A8: { width: 614, height: 874 },
+	A9: { width: 437, height: 614 },
+	A10: { width: 307, height: 437 },
+};
+
 const usePdfWeb = () => {
 	// todo: agregar opcion de tamaño de hoja
 	const ImgToPdf = async (images: ImgToPdf[]) => {
